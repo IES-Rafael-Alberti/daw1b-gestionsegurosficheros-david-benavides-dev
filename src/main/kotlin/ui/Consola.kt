8e78ab6007b42642bdff9126d32094ee85395cc9
dbenfon341 <dbenfon341@g.educaand.es>
2025-03-31 17:37:32 +0200

    Consola terminada

--- src/main/kotlin/ui/Consola.kt
+++ src/main/kotlin/ui/Consola.kt
@@ -1,5 +1,10 @@
 package ui
 
+import jdk.internal.org.jline.reader.EndOfFileException
+import jdk.internal.org.jline.reader.LineReaderBuilder
+import jdk.internal.org.jline.reader.UserInterruptException
+import jdk.internal.org.jline.terminal.TerminalBuilder
+
 /**
  *
  */
@@ -12,43 +17,142 @@ class Consola : IEntradaSalida {
      * Muestra el mensaje por consola. Si salto es true, añade un salto de línea. Si pausa es true, espera a que el usuario pulse Enter.
      */
     override fun mostrar(msj: String, salto: Boolean, pausa: Boolean) {
-        if (salto) println(msj) else print(msj)
-        if (pausa) pausar()
+        if (salto) {
+            println(msj)
+        } else {
+            print(msj)
+        }
+        if (pausa) {
+            pausar()
+        }
     }
 
     /**
      * Muestra el mensaje como un error anteponiendo "ERROR - ". Si ya empieza así, no lo repite.
+     * Si pausar es true, llama al metodo pausar, haciendo una pausa hasta que el usuario presione una tecla.
      */
     override fun mostrarError(msj: String, pausa: Boolean) {
-        println(MENSAJE_ERROR_01 + msj)
-        if (pausa) pausar()
+        if (MENSAJE_ERROR_01 in msj) {
+            println(msj)
+        } else {
+            println(MENSAJE_ERROR_01 + msj)
+        }
+        if (pausa) {
+            pausar()
+        }
     }
 
+    /**
+     *
+     */
     override fun pedirInfo(msj: String): String {
-        TODO("Not yet implemented")
+        if (msj.isNotBlank()) {
+            mostrar(msj, salto = false)
+        }
+        return readln().trim()
     }
 
+    /**
+     *
+     */
     override fun pedirInfo(msj: String, error: String, debeCumplir: (String) -> Boolean): String {
-        TODO("Not yet implemented")
+
+        var input: String
+
+        do {
+            input = pedirInfo(msj)
+            try {
+                require(debeCumplir(input)) { error }
+            } catch (e: IllegalArgumentException) {
+                mostrarError(msj = e.message.toString())
+            }
+        } while (!debeCumplir(input))
+        return input
     }
 
+    /**
+     *
+     */
     override fun pedirDouble(prompt: String, error: String, errorConv: String, debeCumplir: (Double) -> Boolean): Double {
-        TODO("Not yet implemented")
+
+        var input: String
+        var numero: Double? = null
+
+        do {
+            input = pedirInfo(prompt)
+
+            input = input.replace(',', '.')
+
+            try {
+                numero = input.toDoubleOrNull() ?: throw IllegalArgumentException(errorConv)
+                require(debeCumplir(numero)) { error }
+
+            } catch (e: Exception) {
+                mostrarError(e.message.toString())
+            }
+        } while (numero == null)
+        return numero
     }
 
+    /**
+     *
+     */
     override fun pedirEntero(prompt: String, error: String, errorConv: String, debeCumplir: (Int) -> Boolean): Int {
-        TODO("Not yet implemented")
+
+        var input: String
+        var numero: Int? = null
+
+        do {
+            input = pedirInfo(prompt)
+
+            try {
+                numero = input.toIntOrNull() ?: throw IllegalArgumentException(errorConv)
+                require(debeCumplir(numero)) { error }
+
+            } catch (e: Exception) {
+                mostrarError(e.message.toString())
+            }
+        } while (numero == null)
+        return numero
     }
 
+    /**
+     *
+     */
     override fun pedirInfoOculta(prompt: String): String {
-        TODO("Not yet implemented")
+        return try {
+            val terminal = TerminalBuilder.builder()
+                .dumb(true) // Para entornos no interactivos como IDEs
+                .build()
+
+            val reader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .build()
+
+            reader.readLine(prompt, '*') // Oculta la contraseña con '*'
+        } catch (e: UserInterruptException) {
+            mostrarError("Entrada cancelada por el usuario (Ctrl + C).", pausa = false)
+            ""
+        } catch (e: EndOfFileException) {
+            mostrarError("Se alcanzó el final del archivo (EOF ó Ctrl+D).", pausa = false)
+            ""
+        } catch (e: Exception) {
+            mostrarError("Problema al leer la contraseña: ${e.message}", pausa = false)
+            ""
+        }
     }
 
+    /**
+     *
+     */
     override fun pausar(msj: String) {
         println(msj)
         readln()
     }
 
+    /**
+     *
+     */
     override fun limpiarPantalla(numSaltos: Int) {
         if (System.console() != null) {
             mostrar("\u001b[H\u001b[2J", false)
@@ -60,7 +164,17 @@ class Consola : IEntradaSalida {
         }
     }
 
+    /**
+     *
+     */
     override fun preguntar(mensaje: String): Boolean {
-        TODO("Not yet implemented")
+
+        var opcion = ""
+
+        while (opcion != "s" && opcion != "n") {
+            mostrar(mensaje)
+            opcion = readln()
+        }
+        return opcion == "s"
     }
 }
